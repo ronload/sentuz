d0914a0ff8a47eb537ff8de5823275812d35d60a
ronload <91997734+ronload@users.noreply.github.com>
2025-12-06 03:47:35 +0800

    feat(auth): impl gmail auth api

---
+++ src/app/api/auth/gmail/route.ts
@@ -0,0 +1,21 @@
+import { google } from "googleapis";
+import { redirect } from "next/navigation";
+
+export async function GET() {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI,
+  );
+
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: [
+      "https://www.googleapis.com/auth/gmail.modify",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  });
+
+  redirect(authUrl);
+}
